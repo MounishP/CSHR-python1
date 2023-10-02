5df426e10571f2f21d1b1961bb0e38a608b3502d
MounishP <mnsh.pv1@gmail.com>
2023-10-02 12:37:28 +0530

    Day 14

--- clsConstrictor.py
+++ clsConstrictor.py
@@ -1,3 +1,5 @@
+
+
 class Person:
     def __init__(self, name, age):
         self.name = name
@@ -5,10 +7,26 @@ class Person:
 
     def details(self):
         print(f"my name is {self.name} and age is {self.age}")
+
     # name, age
+
+    def get_age(self):
+        return self.age
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        if isinstance(name, str):
+            self.name = name
+        else:
+            print("Name must a string")
 
 
 person1 = Person("Mounish", 28)
 person2 = Person("Tarun", 25)
-person1.details()
-person2.details()
+# person1.details()
+# person2.details()
+print(person1.get_name())
+person1.set_name(765)
+print(person1.get_name())
